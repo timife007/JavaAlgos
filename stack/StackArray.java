3daac717d56850311c33637732ad8da300520b6d
Timothy Ademola <timife007@gmail.com>
2023-10-03 05:19:39 +0100

    error handling in stack

--- stack/StackArray.java
+++ stack/StackArray.java
@@ -5,49 +5,59 @@ public class StackArray {
     private int top;
     private final int[] stack;
 
-    public StackArray(int size){
+    public StackArray(int size) {
         this.maxSize = size;
         this.stack = new int[maxSize];
         this.top = -1;
 
     }
 
-    public void push(int item){
+    public void push(int item) {
         stack[++top] = item; //increment top then add the item at the new position
     }
 
-    public int pop(){
+    public int pop() {
         return stack[top--]; //return the top item, then reduce the top index.
     }
 
-    public int peek(){
+    public int peek() {
         return stack[top];
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return top == -1;
     }
 
-    public boolean isFull(){
+    public boolean isFull() {
         return top == maxSize - 1;
     }
 }
 
-class StackApp{
+class StackApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         StackArray theStack = new StackArray(10);
         theStack.push(20);
         theStack.push(40);
         theStack.push(60);
         theStack.push(80);
 
-        while(!theStack.isEmpty()){
+        while (!theStack.isEmpty()) {
             long value = theStack.pop();
             System.out.println(value);
             System.out.println("  ");
         }
         System.out.println(" ");
+
+
+        StackArray newStack = new StackArray(3);
+        for(int i = 0; i <= 3; i++) {
+            if (!newStack.isFull()) {
+                newStack.push(20 + i);
+            } else {
+                System.out.println("Can't insert, stack is full");
+            }
+        }
     }
 
 }
