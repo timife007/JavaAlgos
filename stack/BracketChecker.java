83101fc80299e1add690686495ea2385d6c55d54
Timothy Ademola <timife007@gmail.com>
2023-10-04 06:24:26 +0100

    input and buffer stream impl and error corrected

--- stack/BracketChecker.java
+++ stack/BracketChecker.java
@@ -1,11 +1,16 @@
 package stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class DelimiterMatching {
+import static stack.ReverseApp.getString;
+
+public class BracketChecker {
     String input;
 
-    DelimiterMatching(String input) {
+    BracketChecker(String input) {
         this.input = input;
     }
 
@@ -19,6 +24,7 @@ public class DelimiterMatching {
             switch (character) {
                 case '{', '[', '(':
                     stackX.push(character);
+                    break;
                 case '}', ']', ')':
                     if (!stackX.isEmpty()) {
                         char charx = stackX.pop();
@@ -26,7 +32,7 @@ public class DelimiterMatching {
                                 character == ')' && charx != '(' ||
                                 character == ']' && charx != '['
                         ) {
-                            System.out.println("error at position :" + i + "with wrong delimiter" + charx);
+                            System.out.println("error at position: " + i + " with wrong delimiter " + character);
                         }
                     } else {
                         //Should not be empty at this point, it's premature
@@ -41,5 +47,27 @@ public class DelimiterMatching {
         if (!stackX.isEmpty()) {
             System.out.println("Error: missing right delimiter, stack should be empty at this point");
         }
+    }
+}
+
+class BracketsApp {
+    public static void main(String[] args) throws IOException {
+        String input;
+        while(true){
+            System.out.println("Enter string containing delimiters: ");
+            System.out.flush();
+            input = getString();
+            if(input.isEmpty())
+                break;
+
+            //Check if valid.
+            BracketChecker bracketChecker = new BracketChecker(input);
+            bracketChecker.check();
+        }
+    }
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
     }
 }
