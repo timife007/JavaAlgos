0086322dc4311113030643c5ea42b96d83231d02
Timothy Ademola <timife007@gmail.com>
2023-10-10 21:39:05 +0100

    linked list values with iteration and recursion

---
+++ linkedlist/LinkedListValues.java
@@ -0,0 +1,48 @@
+package linkedlist;
+
+class LinkedList {
+    static Node head;
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    //Iteration
+    void linkedListValues(Node node){
+        Node current = node;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    //Recursion
+    void linkedListRecurse(Node node){
+        if(node == null) return;
+
+        System.out.println(node.data);
+        linkedListValues(node.next);
+    }
+
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        head = new Node(85);
+        head.next = new Node(15);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(20);
+
+        System.out.println("Given linked list");
+        list.linkedListValues(head);
+        list.linkedListRecurse(head);
+    }
+
+}
